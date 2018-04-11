586dadc34880ce1650083e2a714b25d39363c362
connor <connor@codegood.co>
2018-04-11 21:59:32 +0100

    api url for staging and prod

--- src/globals.ts
+++ src/globals.ts
@@ -1,10 +1,14 @@
 let apiUrl = '';
 
-
 switch (window.location.hostname) {
   case 'localhost':
     apiUrl = 'http://localhost:8181/';
     break;
+  case 'staging.findmydi.com':
+    apiUrl = 'http://staging.api.findmydi.com';
+    break;
+  case 'findmydi.com':
+    apiUrl = 'http://api.findmydi.com';
 }
 
 /**
